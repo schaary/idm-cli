52f45abfc48e4b90764d3be5ac7d8be1b4eb3e7c
Michael Schaarschmidt <michael@schaaryworks.com>
2014-11-03 18:31:48 +0100

    wrapper class for account

---
+++ lib/account.rb
@@ -0,0 +1,12 @@
+# encoding: utf-8
+
+class Account
+   attr_accessor :uid, :email, :firstname, :lastname, :date_of_birth, :gender, :end_date, :final_date, :account_type, :state
+
+  def salutation
+    return 'Sehr geehrter Herr' if 1 == @gender
+    return 'Sehr geehrte Frau'  if 2 == @gender
+    return 'Sehr geehrte(r) Frau/Herr' if 1 != @gender && 2 != @gender
+  end
+
+end
