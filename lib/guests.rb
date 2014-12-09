fa68b80ec6f6f3ee7f4e2a7f3f770080325aff94
Michael Schaarschmidt <michael@schaaryworks.com>
2014-12-09 09:46:23 +0100

    refactor notify method to take states as argument

--- lib/guests.rb
+++ lib/guests.rb
@@ -6,7 +6,7 @@ require 'date'
 require 'date_container'
 require 'summary_mail_handler'
 
-require 'pry'
+# TODO: Das genaue final_date muss noch exakt berechnet werden.
 
 module IdmCLI
   class Guests < Thor
@@ -21,24 +21,29 @@ module IdmCLI
     def send
       DeleteUser::connect
 
-      #open_summary_file
+      open_summary_file
 
       mail_counter = 0
-      mail_counter += notify_after_hour
+      mail_counter += notify state: 'after_hour'
+      mail_counter += notify state: 'suspended'
 
       key_data = {
+        number_of_guest_accounts: plsql.guest_account_pkg.count,
         alive_count: plsql.guest_account_pkg.alive_count,
         after_hour_count: plsql.guest_account_pkg.after_hour_count,
         suspended_count: plsql.guest_account_pkg.suspended_count,
         number_of_mails: mail_counter,
-        final_date: 0
+        final_date: '31.12.2014'
       }
 
       template = "#{DeleteUser::application_root}/templates/status_mails/guest_accounts.erb"
 
+      close_summary_file
+
       mail = SummaryMailHandler.new
       mail.subject "Es wurden #{mail_counter} Abschiedsmails an Gast-Accounts verschickt"
-      mail.to "michael.schaarschmidt@itz.uni-halle.de"
+      mail.to "michael.schaarschmidt@itz.uni-halle.de" #"idm@itz.uni-halle.de"
+      mail.attachment @summary_file_path
       mail.body \
         key_data: key_data,
         template: template
@@ -46,15 +51,19 @@ module IdmCLI
 
     private
 
-    def notify_after_hour
+    def notify state: nil
       date_container = DateContainer.new
 
       records = nil
-      plsql.guest_account_pkg.after_hour { |c| records = c.fetch_all }
+
+      if 'after_hour' == state
+        plsql.guest_account_pkg.after_hour { |c| records = c.fetch_all }
+      elsif 'suspended' == state
+        plsql.guest_account_pkg.suspended { |c| records = c.fetch_all }
+      end
 
       mail_counter = 0
 
-      records.pop(records.length-1)
       records.map do |record|
         account = Account.new
         account.uid = record[3]
@@ -66,10 +75,11 @@ module IdmCLI
         account.account_type = record[6]
         account.state = record[7]
         account.date_of_birth = record[8]
-
+        account.final_date = date_container.final_date
 
         mail = AccountMailHandler.new
         mail.subject "Mitteilung des ITZ der Martin-Luther-Universität Halle-Wittenberg"
+        mail.to account.email
         template = "#{DeleteUser::application_root}/templates/abschiedsbriefe/abschiedsbrief_gaeste.txt"
         send_mail = mail.body \
           account: account,
@@ -78,8 +88,8 @@ module IdmCLI
 
         mail_counter = mail_counter + 1
 
-        write_log_to_file mail: send_mail, account: account
-
+        write_log_to_account_file mail: send_mail, account: account
+        write_log_to_summary_file account: account
         write_log_to_database(
           firstname: account.firstname,
           lastname: account.lastname,
@@ -97,17 +107,23 @@ module IdmCLI
     end
 
     def open_summary_file
-      @summary_file = File.open "#{DeleteUser::application_root}/log/#{Date.today.strftime("%Y%m%d")}_summary.log"
+      @summary_file_path = "#{DeleteUser::application_root}/log/#{Date.today.strftime("%Y%m%d")}_summary_guest.csv"
+      @summary_file = File.open @summary_file_path, "w+"
+      @summary_file.write "'Nutzerkennzeichen','Vorname','Nachname','Mail-Adresse','Ablaufdatum des Accounts','Geplantes Löschdatum'\n"
     end
 
     def close_summary_file
       @summary_file.close
     end
 
-    def write_log_to_file mail: nil, account: nil
+    def write_log_to_account_file mail: nil, account: nil
       file = File.open "#{DeleteUser::application_root}/log/#{Date.today.strftime("%Y%m%d")}_#{account.uid}.log", "w+"
       file.puts mail
       file.close
+    end
+
+    def write_log_to_summary_file account: account
+      @summary_file.write "'#{account.uid}','#{account.firstname}','#{account.lastname}','#{account.email}','#{account.end_date}','#{account.final_date}'\n"
     end
 
     def write_log_to_database(
